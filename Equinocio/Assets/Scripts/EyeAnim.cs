33a65e03cbb6022ea78e204b0416ddec10a97780
JosueRFP <josue.petri.senac@gmail.com>
2025-09-02 13:51:11 -0300

    Animações (quase todas linkadas) e rotação nos objetos 3D/2D

---
+++ Equinocio/Assets/Scripts/EyeAnim.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EyeAnim : MonoBehaviour
+{
+    public static EyeAnim eyeAnim;
+    Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
